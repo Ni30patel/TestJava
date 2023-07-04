43e2f9790f9ec0b2c2aa4aca061cb8485f1cefcd
Nitish Patel <nitish14225@mastek.com>
2023-07-04 16:49:32 +0530

    'pid_changes'

--- src/main/java/com/example/spring/SpringBootDemoApplication.java
+++ src/main/java/com/example/spring/SpringBootDemoApplication.java
@@ -1,14 +1,18 @@
 package com.example.spring;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
-		System.out.println("Second run of spring boot app");
+//		SpringApplication.run(SpringBootDemoApplication.class, args);
+		SpringApplicationBuilder app = new SpringApplicationBuilder(SpringBootDemoApplication.class);
+				app.build().addListeners(new ApplicationPidFileWriter("./bin/shutdown.pid"));
+				app.run();
+				System.out.println("Second run of spring boot app");
 	}
 
 }
