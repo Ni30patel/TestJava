0ead3ea322f77a4430d20267a00bdf90eb0a88a7
Nitish Patel <nitish14225@mastek.com>
2023-07-05 15:58:46 +0530

    'remove_pid_changes'

--- src/main/java/com/example/spring/SpringBootDemoApplication.java
+++ src/main/java/com/example/spring/SpringBootDemoApplication.java
@@ -1,17 +1,16 @@
 package com.example.spring;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(SpringBootDemoApplication.class, args);
-		SpringApplicationBuilder app = new SpringApplicationBuilder(SpringBootDemoApplication.class);
-				app.build().addListeners(new ApplicationPidFileWriter("./bin/shutdown.pid"));
-				app.run();
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+//		SpringApplicationBuilder app = new SpringApplicationBuilder(SpringBootDemoApplication.class);
+//				app.build().addListeners(new ApplicationPidFileWriter("./bin/shutdown.pid"));
+//				app.run();
 				System.out.println("Second run of spring boot app");
 	}
 
